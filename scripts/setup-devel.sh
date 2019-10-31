bfcc3c0f2541c1625dae1a7e142f6f382dcfd746
zduka <zduka@outlook.com>
2019-10-31 22:00:46 +0100

    Update setup info.

--- scripts/setup-devel.sh
+++ scripts/setup-devel.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # clone the additional repositories via ssh
+# NOTE change to https if you do not use ssh keys, or better start using ssh keys:)
 git clone git@github.com:terminalpp/website.git
 git clone git@github.com:terminalpp/bypass.git
 git clone git@github.com:terminalpp/ropen.git
