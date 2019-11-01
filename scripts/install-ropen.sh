34803797b50a7b34e8c8153c70311ebb50ec2565
zduka <zduka@outlook.com>
2019-11-01 22:30:32 +0100

    Added script to install ropen automatically.

---
+++ scripts/install-ropen.sh
@@ -0,0 +1,12 @@
+# fetch subprojects
+bash scripts/setup-repos.sh
+# install dependencies
+bash scripts/setup-linux.sh
+
+# create build dirs and build the application
+mkdir -p build/release
+cd build/release
+
+# build & install the ropen package
+cmake ../.. -DCMAKE_BUILD_TYPE=release -DCMAKE_C_COMPILER=gcc-8 -DCMAKE_CXX_COMPILER=g++-8 -DPACKAGE_INSTALL=tpp-ropen
+sudo cmake --build . --target install
